eb8e517494546d1158094ae9af77451a3a55e9b4
RosslynAsto <rosly_13_5@hotmail.com>
2022-09-16 19:40:38 -0500

    avance1

--- entregable1/src/components/QuoteBox.jsx
+++ entregable1/src/components/QuoteBox.jsx
@@ -6,7 +6,8 @@ const QuoteBox = ({randonQuote}) => {
   return (
     <article className='card'>
         <p className='card__quote'>{randonQuote.quote}</p>
-
+        <h1 className='card__author'>{randonQuote.author}</h1>
+        <button>&#62;</button>
     </article>
   )
 }
